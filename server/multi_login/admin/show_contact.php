2a31cbfe7c1ffb8e44477ae8eab00d03377f1e38
Pham Khoi <khoi.phambkit_5ting@hcmut.edu.vn>
2020-12-29 09:43:32 +0700

    add Internal css and responsive in show_contact.php file

--- server/multi_login/admin/show_contact.php
+++ server/multi_login/admin/show_contact.php
@@ -10,7 +10,7 @@
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) { ?>
             <div class="row d-flex justify-content-center mt-3">
-                <div class="col-sm-8" style="background-color: #ccc;">
+                <div class="col-sm-10" style="background-color: #ccc;">
                     <div class="row border">
                         <div class="col-sm-6 d-flex justify-content-center" data-toggle="collapse" href="#id-<?php echo $row['id'] ?>">
                             <span><b><?php echo $row['date'] ?></b></span>
@@ -21,35 +21,35 @@
                     </div>
                 </div>
 
-                <div class="col-sm-8 collapse multi-collapse" id="id-<?php echo $row['id'] ?>">
+                <div class="col-sm-10 collapse multi-collapse" id="id-<?php echo $row['id'] ?>">
                     <div class="row border">
                         <div class="col-sm-12">
                             <div class="row m-t1">
-                                <div class="col-sm-3">
+                                <div class="col-sm-2 col-2" style="min-width: 140px;">
                                     <span><b>Tên người gửi:</b></span>
                                 </div>
-                                <div class="col-sm-3">
+                                <div class="col-sm-3 col-3" style="min-width: 148px;">
                                     <span><?php echo $row['last_name'] . ' ' . $row['first_name'] ?></span>
                                 </div>
                             </div>
 
                             <div class="row mt-1">
-                                <div class="col-sm-6">
+                                <div class="col-lg-6 col-sm-12" style="padding-right: 0;">
                                     <div class="row">
-                                        <div class="col-sm-6">
+                                        <div class="col-sm-1 col-1" style="min-width: 71px;">
                                             <span><b>Email:</b></span>
                                         </div>
-                                        <div class="col-sm-6">
+                                        <div class="col-sm-3 col-3" style="padding-left: 0;">
                                             <a href="https://mail.google.com/mail/?view=cm&fs=1&to=<?php echo $row['email'];?>&su=SUBJECT&body=BODY" target="_blank" style="color: blue;"><?php echo $row['email'] ?></a>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-sm-6">
+                                <div class="col-lg-6  col-sm-12">
                                     <div class="row">
-                                        <div class="col-sm-6 d-flex justify-content-end">
+                                        <div class="col-sm-3 col-3 d-flex" style="min-width: 135px;">
                                             <span><b>Số điện thoại:</b></span>
                                         </div>
-                                        <div class="col-sm-6">
+                                        <div class="col-sm-6 col-6" style="min-width: 135px;">
                                             <span style="color: black;"><?php echo $row['phone_number'] ?></span>
                                         </div>
                                     </div>
